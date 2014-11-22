5848807442e2576f252d4bfb7a69e9e83e2eb4b8
JSenou <coxxx328@umn.edu>
2014-11-22 15:35:02 -0600

    rankhospital.R has choosen hospitals with specified ranking but I want to refine the code so there is less redundancies

--- rprog-data-ProgAssignment3-data/rankhospital.R
+++ rprog-data-ProgAssignment3-data/rankhospital.R
@@ -6,20 +6,52 @@ rankhospital <- function(state, outcome, num){
         ## Output: Char vector of first hospital in that rank category
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character") # reading the file
         subset_state <- data[which(data$State == state), ] # contains all data but only for "state"
-        row_num <- nrow(subset_state)
-        values <- numeric() # this vector will hold all values from state and outcome
         outcome_col <- numeric() # this will hold the col number assciated to outcome
+        values <- numeric() # this vector will hold all values from state and outcome
         if(outcome == "heart attack"){
                 outcome_col <- 11
-                for(i in 1:row_num){
+                name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack" 
+                for(i in 1:nrow(subset_state)){
                         values[i] <- subset_state[i, outcome_col] # collecting all values for outcome
                         values <- unique(sort(as.numeric(values))) # ascending sort, removal of NAs and nique values of values vec
                 }
-                
+                rank_value <- values[num] # the value of outcome for the rank specified by num
+                hospital <- subset_state[which(subset_state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == rank_value),]
+                hospital <- sort(hospital[,"Hospital.Name"])
         }else if(outcome == "heart failure"){
                 outcome_col <- 17
+                name <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"  
+                for(i in 1:nrow(subset_state)){
+                        values[i] <- subset_state[i, outcome_col] # collecting all values for outcome
+                        values <- unique(sort(as.numeric(values))) # ascending sort, removal of NAs and nique values of values vec
+                }   
+                rank_value <- values[num] # the value of outcome for the rank specified by num
+                hospital <- subset_state[which(subset_state$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == rank_value),]
+                hospital <- sort(hospital[,"Hospital.Name"])
         }else if(outcome ==  "pneumonia"){
                 outcome_col <- 23
+                name <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" 
+                for(i in 1:nrow(subset_state)){
+                        values[i] <- subset_state[i, outcome_col] # collecting all values for outcome
+                        values <- unique(sort(as.numeric(values))) # ascending sort, removal of NAs and nique values of values vec
+                }
+                rank_value <- values[num] # the value of outcome for the rank specified by num
+                hospital <- subset_state[which(subset_state$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == rank_value),]
+                hospital <- sort(hospital[,"Hospital.Name"])
         }
+       
+#         for(i in 1:nrow(subset_state)){
+#                 values[i] <- subset_state[i, outcome_col] # collecting all values for outcome
+#                 values <- unique(sort(as.numeric(values))) # ascending sort, removal of NAs and nique values of values vec
+#         }
+#        rank_value <- values[num] # the value of outcome for the rank specified by num
+#        hospital <- subset(subset_state, name == rank_value) #character() # vector will hold the names of the hospitals of the specified ranking
+#         for(i in 1:nrow(subset_state)){
+#                 if(subset_state[i, outcome] == rank_value){
+#                         hospital[i] <- subset_state[i, "Hospital.Name"]
+#                 }
+#         }
         print(values)
+        print(length(values))
+        hospital
 }
