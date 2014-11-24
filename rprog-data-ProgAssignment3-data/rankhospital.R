33b9da0476c6785c2b4b53f32a236b4fe40aa96a
JSenou <coxxx328@umn.edu>
2014-11-23 19:38:17 -0600

    almost have it working, but the if loop is not evaluating T/F and I'm not sure why

--- rprog-data-ProgAssignment3-data/rankhospital.R
+++ rprog-data-ProgAssignment3-data/rankhospital.R
@@ -1,9 +1,10 @@
-
 rankhospital <- function(state, outcome, num){
+        
         ## The purpose of this function is to find hospitals of a certain ranking = num
         ## Input: 2 letter state abbreviation, outcome = one of ("heart attack", "heart failure", "pneumonia"),
         ## num which is an integer ranking or "worst" or "best"
         ## Output: Char vector of first hospital in that rank category
+        
         data <- read.csv("outcome-of-care-measures.csv", colClasses = "character") # reading the file
         subset_state <- data[which(data$State == state), ] # contains all data but only for "state"
         outcome_col <- numeric() # this will hold the col number assciated to outcome
@@ -21,21 +22,31 @@ rankhospital <- function(state, outcome, num){
                 name <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" 
                 all_hospitals <- subset_state[ , c("Hospital.Name", name)]
         }
-        for(i in 1:nrow(subset_state)){
-                values[i] <- subset_state[i, outcome_col] # collecting all values for outcome
+        for(i in 1:nrow(all_hospitals)){
+                values[i] <- all_hospitals[i, 2] # collecting all values (including repetitions) for outcome
                 values <- sort(as.numeric(values)) # ascending sort, removal of NAs
+        }
+        if(num == "best"){
+                num <- 1
+        }else if(num == "worst"){
+                num <- length(values)
         }
         rank_value <- values[num] # the value of outcome for the rank specified by num
         hospitals <- character()  # this is where the relavant hospitals will be stored
-        j <- 1
+        print(rank_value)
+        all_hospitals[ , 2] <- as.numeric(all_hospitals[ , 2])
         for(i in 1:nrow(all_hospitals)){
+                print(i)
+                print(all_hospitals[i,2])
                  if(all_hospitals[i,2] == rank_value){
-                         hospitals[j] <- all_hospitals[i,"Hospital.Name"]
-                         j <- j + 1
+                         hospitals[i] <- all_hospitals[i,"Hospital.Name"]
                  }
          }
+        print(values)
         hospitals <- sort(hospitals)
         source("index_hospital.R")
         index <- index_hospital(values, num)
+        print(rank_value)
+        print(hospitals)
         hospitals[index]
 }
